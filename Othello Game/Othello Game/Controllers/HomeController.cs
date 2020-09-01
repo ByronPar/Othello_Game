d26d095c36fe361f0f8ad54e2146f42ef4f19d8c
Byron Josué Par Rancho <3237971560511@ingenieria.usac.edu.gt>
2020-08-31 23:44:13 -0600

    login y registro look and feel completo

--- Othello Game/Othello Game/Controllers/HomeController.cs
+++ Othello Game/Othello Game/Controllers/HomeController.cs
@@ -13,16 +13,13 @@ namespace Othello_Game.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Registro()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
