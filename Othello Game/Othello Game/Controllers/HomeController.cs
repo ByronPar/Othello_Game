38baf078b98eb142223a212b20ff7c7ee9a9ade3
Byron Josué Par Rancho <3237971560511@ingenieria.usac.edu.gt>
2020-09-09 15:24:31 -0600

    modificando login

--- Othello Game/Othello Game/Controllers/HomeController.cs
+++ Othello Game/Othello Game/Controllers/HomeController.cs
@@ -1,27 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Othello_Game.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Othello_Game.Controllers
 {
     public class HomeController : Controller
     {
+        private Othello_GameEntities db = new Othello_GameEntities();
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult Login()
+        //
+        // GET: /Home/Login
+        [AllowAnonymous]
+        public ActionResult Login(string message = "")
         {
+            ViewBag.Message = message;
             return View();
+        }
+
+        //
+        // POST: /Home/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string contrasenia)
+        {
+            var jugador = db.Jugador.FirstOrDefault(e => e.id_usuario == username && e.contrasenia == contrasenia);
+            if (jugador != null)
+            {
+                return View(jugador);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Home", "El usuario no existe o la contraseña es incorrecta");
+            }
+
         }
 
         public ActionResult Registro()
         {
 
             return View();
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Othello Game/Othello Game/Controllers/JugadorController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Othello_Game.Models;
+
+namespace Othello_Game.Controllers
+{
+    public class JugadorController : Controller
+    {
+        private Othello_GameEntities db = new Othello_GameEntities();
+
+        // GET: Jugador
+        public ActionResult Index()
+        {
+            var jugador = db.Jugador.Include(j => j.Pais);
+            return View(jugador.ToList());
+        }
+
+        // GET: Jugador/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugador jugador = db.Jugador.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // GET: Jugador/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_pais = new SelectList(db.Pais, "id_pais", "nombre");
+            return View();
+        }
+
+        // POST: Jugador/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_usuario,nombres,apellidos,contrasenia,fecha_nacimiento,correo,id_pais")] Jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jugador.Add(jugador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_pais = new SelectList(db.Pais, "id_pais", "nombre", jugador.id_pais);
+            return View(jugador);
+        }
+
+        // GET: Jugador/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugador jugador = db.Jugador.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_pais = new SelectList(db.Pais, "id_pais", "nombre", jugador.id_pais);
+            return View(jugador);
+        }
+
+        // POST: Jugador/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_usuario,nombres,apellidos,contrasenia,fecha_nacimiento,correo,id_pais")] Jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jugador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_pais = new SelectList(db.Pais, "id_pais", "nombre", jugador.id_pais);
+            return View(jugador);
+        }
+
+        // GET: Jugador/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jugador jugador = db.Jugador.Find(id);
+            if (jugador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jugador);
+        }
+
+        // POST: Jugador/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Jugador jugador = db.Jugador.Find(id);
+            db.Jugador.Remove(jugador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
