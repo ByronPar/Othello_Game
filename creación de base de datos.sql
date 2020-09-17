ee649ef04259723ab02f43169b9cd6b906c08804
Byron Josué Par Rancho <3237971560511@ingenieria.usac.edu.gt>
2020-09-17 11:09:24 -0600

    update database

--- creación de base de datos.sql
+++ creación de base de datos.sql
@@ -16,56 +16,40 @@ CREATE TABLE Jugador(
 	constraint FK_Pais foreign key (id_pais) references Pais(id_pais)
 );
 
-CREATE TABLE Torneo(
-	id_torneo INT IDENTITY(1,1) PRIMARY KEY,
-	nombre VARCHAR(200) NOT NULL,
-	id_GanadorTorneo VARCHAR(200),
-	constraint FK_GanadorTorneo FOREIGN KEY (id_GanadorTorneo) references Jugador(id_usuario)
-);
-
 CREATE TABLE Tipo_Partida(
 	id_tipo_partida INT IDENTITY(1,1) PRIMARY KEY,
 	nombre VARCHAR(50) NOT NULL,
 );
 
+CREATE TABLE Torneo(
+	id_torneo INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR(200) NOT NULL,
+	id_GanadorTorneo VARCHAR(200) NULL
+);
+
 CREATE TABLE Partida(
 	id_partida INT IDENTITY(1,1) PRIMARY KEY,
-	color_siguiente VARCHAR(50) NOT NULL,
-	id_GanadorPartida VARCHAR(200),
-	id_tipo_partida INT NOT NULL
-	constraint FK_GanadorPartida FOREIGN KEY (id_GanadorPartida) references Jugador(id_usuario),
-	constraint FK_Tipo_Partida FOREIGN KEY (id_tipo_partida) references Tipo_Partida(id_tipo_partida)
-);
-
-CREATE TABLE Ficha(
-	id_ficha INT IDENTITY(1,1) PRIMARY KEY,
-	color VARCHAR(50) NOT NULL,
-	columna VARCHAR(5) NOT NULL,
-	fila INT NOT NULL,
-	id_partida INT NOT NULL,
-	constraint FK_FichaPartida FOREIGN KEY (id_partida) references Partida(id_partida)
-);
-
-CREATE TABLE Jugador_Partida(
-	id_jugador_partida INT IDENTITY(1,1) PRIMARY KEY,
 	movimientos INT NOT NULL,
-	color VARCHAR(50) NOT NULL,
+	color VARCHAR(50)NOT NULL,
 	cantidad_fichas INT NOT NULL,
+	ganador VARCHAR(10) ,
 	id_usuario VARCHAR(200) NOT NULL,
-	id_partida INT NOT NULL,
+	id_tipo_partida INT NOT NULL,
 	id_jugador_2 INT,
 	constraint FK_UsuarioPartida FOREIGN KEY (id_usuario) references Jugador(id_usuario),
-	constraint FK_PartidaUsuario FOREIGN KEY (id_partida) references Partida(id_partida),
-	constraint FK_jugador2 FOREIGN KEY (id_jugador_2) references Jugador_Partida(id_jugador_partida)
+	constraint FK_Tipo_Partida FOREIGN KEY (id_tipo_partida) references Tipo_partida(id_tipo_partida),
+	constraint FK_jugador2 FOREIGN KEY (id_jugador_2) references Partida(id_partida)
 );
 
 CREATE TABLE Torneo_Partida(
 	id_torneo_partida INT IDENTITY(1,1) PRIMARY KEY,
+	no_ronda INT NOT NULL,
 	id_torneo INT NOT NULL,
-	id_jugador_partida INT NOT NULL,
-	constraint FK_Torneo FOREIGN KEY (id_torneo) references Torneo(id_torneo),
-	constraint FK_Jugador_Partida FOREIGN KEY (id_jugador_partida) references Jugador_Partida(id_jugador_partida)
+	id_partida INT NOT NULL,
+	constraint FK_TorneoPartida FOREIGN KEY (id_torneo) references Torneo(id_torneo),
+	constraint FK_PartidaTorneo FOREIGN KEY (id_partida) references Partida(id_partida)
 );
+
 
 --------- insertando paises que existiran dentro de la aplicación
 
