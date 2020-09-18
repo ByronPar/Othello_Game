777a3b9dc27af3795a2dde7ff484c44004a27345
Byron Josué Par Rancho <3237971560511@ingenieria.usac.edu.gt>
2020-09-18 00:06:27 -0600

    update othello 2

--- OthelloGame_2/OthelloGame_2/Controllers/HomeController.cs
+++ OthelloGame_2/OthelloGame_2/Controllers/HomeController.cs
@@ -1,30 +1,53 @@
-﻿using System;
+﻿using OthelloGame_2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace OthelloGame_2.Controllers
 {
     public class HomeController : Controller
     {
+        private DataBase db = new DataBase();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        // GET: /Home/Login
+        [HttpGet]
+        public ActionResult Login(string message = "")
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = message;
             return View();
         }
 
-        public ActionResult Contact()
+        //
+        // POST: /Home/Login
+        [HttpPost]
+        public ActionResult Login(string username, string contrasenia)
         {
-            ViewBag.Message = "Your contact page.";
+            var jugador = db.Jugador.FirstOrDefault(e => e.id_usuario == username && e.contrasenia == contrasenia);
+            if (jugador != null)
+            {
+                FormsAuthentication.SetAuthCookie(jugador.id_usuario, true);
+                return RedirectToAction("Detalles", "Jugador");
+            }
+            else
+            {
 
-            return View();
+                return RedirectToAction("Login", "Home", new { message = "El usuario no existe o la contraseña es incorrecta" });
+            }
+
+        }
+        //cerrar sesión
+        [Authorize]
+        public ActionResult CerrarSesion()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
         }
     }
 }
